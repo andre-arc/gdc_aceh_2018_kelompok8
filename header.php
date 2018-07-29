4b0022a27986d56f06625c47573178bb1185bea4
Pakdhe Febri <johndoe@example.com>
2018-07-29 16:14:34 +0700

    top nav edited

--- header.php
+++ header.php
@@ -12,9 +12,9 @@
         <div id="navbar" class="navbar-collapse navbar-right collapse">
           <ul class="nav navbar-nav">					
                 <li><a href="index.php"><i class="fa fa-bus"></i>&nbsp;Home</a></li>
-                <li><a href="#"><i class="fa fa-bus"></i>&nbsp;Klasemen</a></li>
-                <li><a href="#"><i class="fa fa-bus"></i>&nbsp;Halte</a></li>						
-                <li><a href="#"><i class="fa fa-cogs"></i>&nbsp;Login/Register</a></li>						
+                <li><a href="klasemen.php"><i class="fa fa-bus"></i>&nbsp;Klasemen</a></li>
+                <li><a href="halte.php"><i class="fa fa-bus"></i>&nbsp;Halte</a></li>						
+                <li><a href="login-register.php"><i class="fa fa-cogs"></i>&nbsp;Login/Register</a></li>						
 					</ul>
         </div>
       </div>
