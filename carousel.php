4bf59f91b36ccf54c3a503529901fc8f95e8c552
Pakdhe Febri <johndoe@example.com>
2018-07-29 15:46:24 +0700

    bootstrap 3 basic

---
+++ carousel.php
@@ -0,0 +1,6 @@
+    <div class="jumbotron">
+      <div class="container">
+        <h2>TransKoetaradja<strong>Lover</strong></h2>
+        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+      </div>
+    </div>
---
+++ header.php
@@ -0,0 +1,21 @@
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php"><i class="fa fa-bus"></i>&nbsp;TransKoetaradja<strong>Lover</strong></a>
+        </div>
+        <div id="navbar" class="navbar-collapse navbar-right collapse">
+          <ul class="nav navbar-nav">					
+                <li><a href="index.php"><i class="fa fa-bus"></i>&nbsp;Home</a></li>
+                <li><a href="#"><i class="fa fa-bus"></i>&nbsp;Klasemen</a></li>
+                <li><a href="#"><i class="fa fa-bus"></i>&nbsp;Halte</a></li>						
+                <li><a href="#"><i class="fa fa-cogs"></i>&nbsp;Login/Register</a></li>						
+					</ul>
+        </div>
+      </div>
+    </nav>
